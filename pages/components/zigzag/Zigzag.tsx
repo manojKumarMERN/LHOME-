421439c2d3d670df4a99c6998d78695a8bfd0244
hackerkhan001 <ggboykan1426@gmail.com>
2023-11-02 14:40:51 +0530

    2.40

--- pages/components/zigzag/Zigzag.tsx
+++ pages/components/zigzag/Zigzag.tsx
@@ -1,44 +1,41 @@
-import React from "react";
-import { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { simpleCallInitAPI } from '../../../services/ApicallInit';
 import * as config from "../../../next.config";
 import css from './zigzag.module.scss';
+
 function Zigzag() {
-    const [animationGif, setAnimationGif] = React.useState([]);
-    const [isAnimationPaused, setIsAnimationPaused] = React.useState(false);
+    const [animationGif, setAnimationGif] = useState([]);
+    const [isAnimationPaused, setIsAnimationPaused] = useState(false);
     let assetpath = config.assetPrefix ? `${config.assetPrefix}` : ``;
 
-    React.useEffect(() => {
+    useEffect(() => {
         let api = simpleCallInitAPI(`${assetpath}/assets/zigzag.json`);
-
-        api.then((data: any) => {
-            let AnimationGifImage = [];
-            console.log(data);
-            data?.data?.zigzag?.forEach((datas: any) => {
-                let lc: any = {};
-                lc.AnimationImage = `${assetpath}${datas.image}`;
-                lc.AnimationAlt = `${assetpath}${datas.alt}`;
-                AnimationGifImage.push(lc);
-            });
-            setAnimationGif(AnimationGifImage);
-
+    
+        api.then((data) => {
+            console.log("Received data:", data);
+            setAnimationGif(data?.data?.zigzag || []);
         });
     }, []);
+
     const handleMouseEnter = () => {
+        console.log("Mouse entered");
         setIsAnimationPaused(true);
     };
 
     const handleMouseLeave = () => {
+        console.log("Mouse left");
         setIsAnimationPaused(false);
     };
+    console.log(animationGif);
+    console.log("isAnimationPaused:", isAnimationPaused);
 
     return (
         <Fragment>
-            {animationGif.map((datas: any) => (
-                <div>
+            {animationGif.map((datas, index) => (
+                <div key={index}>
                     <img
-                        src={isAnimationPaused ? datas.AnimationImage : datas.StaticImage}
-                        alt={datas.AnimationAlt}
+                        src={isAnimationPaused ? datas["image"] : datas["image1"]}
+                        alt={datas.alt}
                         className={css.gif_image}
                         onMouseEnter={handleMouseEnter}
                         onMouseLeave={handleMouseLeave}
@@ -48,4 +45,5 @@ function Zigzag() {
         </Fragment>
     )
 }
+
 export default Zigzag;
