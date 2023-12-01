b5a57c3c23a091dd67c328651a2cfa9f3ccb1f59
hackerkhan001 <ggboykan1426@gmail.com>
2023-12-01 17:52:10 +0530

    last update kalai

--- pages/components/SelectButton/Bookfreedropdown.tsx
+++ pages/components/SelectButton/Bookfreedropdown.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import {Input} from 'reactstrap';
-export function Bookfreedropdown() {
-    const [title, setTitle] = React.useState("");
+import { Input } from 'reactstrap';
+import css from './Selectbutton.module.scss'
+import { MdLocationPin } from "react-icons/md";
+interface districtprops {
+    district: string[];
+    heading: string;
+}
+const Bookfreedropdown: React.FC<districtprops> = ({district,heading}) => {
+    console.log(district);
+    const [districtList, setDistrictList] = React.useState("");
     return (
         <>
-            <div>
+            <div className={css.bookfree_dropdown}>
+                <p className={css.select_button_Heading} style={{paddingTop:'4%'}}>{heading}</p>
+                <span className={heading === "My Locality is"? css.show : css.hidden}><MdLocationPin/></span>
                 <Input
                     type="select"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
+                    value={districtList}
+                    onChange={(e) => setDistrictList(e.target.value)}
                     id="selectTitle"
+                    className={css.input_Dropdown}
                 >
-                    <option hidden>Title</option>
-                    <option value="Ms.">Ms.</option>
-                    <option value="Mr.">Mr.</option>
+                    <option hidden>Location</option>
+                    {district && district.map((item, index) => (
+                        <option key={index} value={item}>
+                            {item}
+                        </option>
+                    ))}
                 </Input>
             </div>
         </>
     )
 }
+export default Bookfreedropdown;
