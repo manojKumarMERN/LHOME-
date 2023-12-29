4b3517f2bd6b4f352568eeb2328fa70d59f56df6
Selvakumar G <selvakumar.g@invenzolabs.com>
2023-12-29 13:07:06 +0530

    new changes

--- pages/getQuote.tsx
+++ pages/getQuote.tsx
@@ -10,11 +10,7 @@ import { useRouter } from 'next/router';
 import Footer from "./components/Footer/Footer";
 import GetQuoteContent from "./components/getQuote/getQuote_content";
 import { Grid } from "@mui/material";
-interface homeproperties {
-    screenwidth: number;
-    screenheight: number;
-  
-  }
+
 
  const contentDatas =  [
     {
@@ -37,13 +33,72 @@ interface homeproperties {
     },
  ]
 
-const GetQuote:React.FC<homeproperties> = ({ screenwidth, screenheight })=>{
+const GetQuote:React.FC = ()=>{
     let assetpath = config.assetPrefix ? `${config.assetPrefix}` : ``;
   
     const page =React.useRef<HTMLDivElement>(null);;
 
     const [prevPosition, setPrev] = React.useState(0);
     const [hidden, setHidden] = React.useState(false);
+    const [screenwidth, setWidth] = React.useState(window.innerWidth);
+    let hgtt = 0;
+    if (window.innerWidth < 600) {
+        hgtt = window.innerHeight - 210;
+        if (window.innerWidth > 490 && window.innerWidth < 512) {
+            hgtt += 10;
+        }
+    } else {
+        hgtt = window.innerHeight - 160;
+    }
+    const [screenheight, setHeight] = React.useState(hgtt);
+
+
+    const handleResize = React.useCallback(() => {
+        setWidth(window.innerWidth);
+        let hgtt = 0;
+        if (window.innerWidth < 600) {
+            hgtt = window.innerHeight - 210;
+            if (window.innerWidth > 490 && window.innerWidth < 512) {
+                hgtt += 10;
+            }
+            if (window.innerWidth > 571 && window.innerWidth < 599) {
+                hgtt += 50;
+            }
+            if (window.innerWidth > 570 && window.innerWidth < 572) {
+                hgtt += 45;
+            }
+            if (window.innerWidth > 509 && window.innerWidth < 571) {
+                hgtt += 25;
+            }
+            if (window.innerWidth > 500 && window.innerWidth < 510) {
+                hgtt += 15;
+            }
+            if (window.innerWidth < 500) {
+                hgtt -= 10;
+            }
+        } else {
+            hgtt = window.innerHeight - 160;
+        }
+        setHeight(hgtt);
+    }, []);
+
+    const handleResized = React.useCallback(() => {
+        setTimeout(() => {
+            handleResize();
+        }, 1000);
+    }, [handleResize]);
+
+    
+    React.useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResized);
+    }, [handleResize, handleResized]);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            handleResize();
+        }, 500);
+    }, [handleResize]);
   
     const pageheaderMonitor = () => {
         if (page.current.scrollTop > prevPosition) {
@@ -56,6 +111,7 @@ const GetQuote:React.FC<homeproperties> = ({ screenwidth, screenheight })=>{
         }
     }
     const router = useRouter();
+    
 
     return (
         <React.Fragment>
@@ -68,7 +124,7 @@ const GetQuote:React.FC<homeproperties> = ({ screenwidth, screenheight })=>{
 
                     <div ref={page} onScroll={pageheaderMonitor} className={hidden ? css.LhomeBottom1 : css.LhomeBottom}>
                         <div className="text-[#222222] text-[12px] sm:text-[24px] larger:text-[1.5vw] py-[1.5%] text-center leading-7 font-semibold">Below are your estimates to book a free consultation</div>
-                        <Grid container justifyContent={"center"} paddingY={2} paddingX={10} gap={10}  >
+                        <Grid container justifyContent={"center"} paddingY={2} paddingX={{xs : 2 , sm:10}} gap={10}  >
                         {
                             contentDatas.map((data , index)=>(
                                 <Grid item lg={3} md={3.8} sm={12} key={index}>
