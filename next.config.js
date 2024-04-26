64f57e0bed26bfde1d4bdf1422ea1a1431798081
JananiR1299 <jananiravindran1299@gmail.com>
2024-04-26 10:39:19 +0530

    Deploy Change

--- next.config.js
+++ next.config.js
@@ -1,22 +1,44 @@
+/** @type {import('next').NextConfig} */
 const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? 'https://www.lhome.co.in' : '/',
+  assetPrefix: '/',
   images: {
-    domains: isProd ? ['www.lhome.co.in'] : ['localhost'],
+    domains: isProd ? ['https://www.lhome.co.in'] : ['localhost'],
     unoptimized: isProd,
   },
   distDir: 'dist',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  output: isProd ? 'export' : undefined,
+  assetPrefix: isProd ? 'https://www.lhome.co.in' : undefined,
   trailingSlash: true,
 };
 
 module.exports = nextConfig;
+
+// const path = require('path');
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   assetPrefix: isProd ? 'https://www.lhome.co.in' : '/',
+//   images: {
+//     domains: isProd ? ['www.lhome.co.in'] : ['localhost'],
+//     unoptimized: isProd,
+//   },
+//   distDir: 'dist',
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   trailingSlash: true,
+// };
+
+// module.exports = nextConfig;
 
 
 // /** @type {import('next').NextConfig} */
